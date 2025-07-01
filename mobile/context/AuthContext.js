5806d1312efb016f046dcf14f35a8aaeb4830c2d
Sydney <jimenezsydney07@gmail.com>
2025-07-01 11:57:15 +0800

    feat: added a custom login/registration page and organized files

---
+++ mobile/context/AuthContext.js
@@ -0,0 +1,25 @@
+import React, { createContext, useState, useEffect } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}; 
