1c98fdc54a30a42d9837e8afd44e18473a63a81d
Sydney <zeeddd0107>
2025-07-01 11:57:15 +0800

    feat: added a custom login/registration page and organized files

---
+++ mobile/src/authService.js
@@ -0,0 +1,16 @@
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import app from "./firebase";
+
+const auth = getAuth(app);
+
+export const registerUser = (email, password) => {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const loginUser = (email, password) => {
+  return signInWithEmailAndPassword(auth, email, password);
+}; 
