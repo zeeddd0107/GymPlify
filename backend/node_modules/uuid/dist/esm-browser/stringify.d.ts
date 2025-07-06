43438b6b1fa94e63524ed25c8d5bcb4a1d344540
Sydney <jimenezsydney07@gmail.com>
2025-07-07 00:24:12 +0800

    comment out .env files in .gitignore temporarily

---
+++ backend/node_modules/uuid/dist/esm-browser/stringify.d.ts
@@ -0,0 +1,6 @@
+export declare function unsafeStringify(
+  arr: Uint8Array,
+  offset?: number,
+): string;
+declare function stringify(arr: Uint8Array, offset?: number): string;
+export default stringify;
