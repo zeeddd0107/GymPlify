99edfc2f80bb8117aa781b20984a4040f0da4d9c
Sydney <jimenezsydney07@gmail.com>
2025-07-17 01:54:14 +0800

    feat(mobile): add Google Sign-In functionality using Expo Auth Session

--- mobile/app/auth/index.jsx
+++ mobile/app/auth/index.jsx
@@ -6,9 +6,15 @@ import {
   Pressable,
   StyleSheet,
   ActivityIndicator,
+  Button
 } from "react-native";
 import { loginUser, registerUser } from "@/src/authService";
 import { useRouter } from "expo-router";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function AuthScreen() {
   const [mode, setMode] = useState("login"); // 'login' or 'register'
@@ -17,6 +23,44 @@ export default function AuthScreen() {
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
   const router = useRouter();
+  // Google Auth State
+  const [userInfo, setUserInfo] = React.useState(null);
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    androidClientId: "92567912347-q242e9l92m353hu4h07f4dm5hpvbjdal.apps.googleusercontent.com",
+    iosClientId: "92567912347-942835ebp1tv39tsj52s0s93tlaou44a.apps.googleusercontent.com",
+    webClientId: "792567912347-ub4fb24e5ne43litim6ql4c3tq03dqfa.apps.googleusercontent.com",
+  });
+
+  React.useEffect(() => {
+    handleGoogleLogin();
+  }, [response]);
+
+  async function handleGoogleLogin() {
+    const user = await AsyncStorage.getItem("@user");
+    if (!user) {
+      if(response?.type === "success") {
+        await getUserInfo(response.authentication.accessToken);
+      }
+    } else {
+      setUserInfo(JSON.parse(user));
+    }
+  }
+
+  const getUserInfo = async (token) => {
+    if (!token) return;
+    try {
+      const response = await fetch("https://www.googleapis.com/userinfo/v2/me", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const user = await response.json();
+      await AsyncStorage.setItem("@user", JSON.stringify(user));
+      setUserInfo(user);
+    } catch (error) {
+      // Handle error
+    }
+  };
 
   const handleAuth = async () => {
     setLoading(true);
@@ -37,6 +81,11 @@ export default function AuthScreen() {
 
   return (
     <View style={styles.container}>
+      <Text>
+        {JSON.stringify(userInfo, null, 2)}
+      </Text>
+      <Button title="Sign in with Google" onPress={() => promptAsync()} />
+      <Button title="Delete local storage" onPress={() => AsyncStorage.removeItem("@user")} />
       <Text style={styles.title}>
         GymPlify {mode === "login" ? "Login" : "Register"}
       </Text>
