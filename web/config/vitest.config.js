4fcc76142ced3c3bb7086d924303c2c003f53c58
Sydney <jimenezsydney07@gmail.com>
2025-10-13 20:46:01 +0800

    test: Enable globals in Vitest configuration Updated Vitest config to use global test APIs for easier test writing.

--- web/config/vitest.config.js
+++ web/config/vitest.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom',
-    setupFiles: ['./src/utils/test-setup.js'],
+    environment: "jsdom",
+    setupFiles: ["./src/utils/test-setup.js"],
+    globals: true,
   },
-})
+});
